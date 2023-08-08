00ac043eba3eba61e5ea9c97ca8f59e4bbb36fca
Arkadiusz Modrzejewski <arekmod98@gmail.com>
2023-08-08 23:35:08 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <book-item v-for='book in storedBooks'
+               :key='book.id'
+               :title='book.title'
+               :description='book.description'
+               :link='book.link'
+    ></book-item>
+  </ul>
+</template>
+
+<script>
+import BookItem from '@/components/BookItems/BookItem.vue';
+
+export default {
+  components: { BookItem },
+  data() {
+    return {
+      storedBooks: [
+        {
+          id: 'first-book',
+          title: 'First Book',
+          description: 'The first added book.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'second-book',
+          title: 'Second Book',
+          description: 'The second added book.',
+          link: 'https://vuejs.org'
+        }
+      ]
+    };
+  }
+};
+</script>
